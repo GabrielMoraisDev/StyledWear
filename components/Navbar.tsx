c5dafb35246b4af94c3bd56d29299f90c4f7d99b
GabrielMoraisDev <leiteetec@gmail.com>
2024-09-23 20:56:43 -0300

    Process Development

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar(){
     return(
-        <div className="xl:fixed xl:top-0 xl:h-12 xl:w-full bg1 z-50">
+        <div className="xl:fixed xl:top-0 xl:h-24 xl:w-full bg1 z-50">
             <h1>Navbar</h1>
         </div>
     )
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -6,13 +6,29 @@ import Image from "next/image";
 
 export function Slider() {
   return (
-    <div className="h-[24rem] mt-10">
-      <Carousel>
-      <div className="flex justify-center bg-slate-100 w-full"><Image width={1920} height={1080} className="h-auto w-auto" src="/img/1.png" alt="..." /></div> 
-      <div className="flex justify-center bg-slate-100 w-full"><Image width={1920} height={1080} className="h-auto w-auto" src="/img/2.png" alt="..." /></div> 
-      <div className="flex justify-center bg-slate-100 w-full"><Image width={1920} height={1080} className="h-auto w-auto" src="/img/3.png" alt="..." /></div> 
-      <div className="flex justify-center bg-slate-100 w-full"><Image width={1920} height={1080} className="h-auto w-auto" src="/img/4.png" alt="..." /></div> 
-      <div className="flex justify-center bg-slate-100 w-full"><Image width={1920} height={1080} className="h-auto w-auto" src="/img/5.png" alt="..." /></div> 
+    <div className="h-[20rem] mt-9 bg-slate-100">
+      <Carousel className="rounded-none">
+
+      <div className="flex justify-center bg-slate-100 w-full h-full">      
+        <Image width={1920} height={1080} className="h-auto w-1/2 mt-0" src="/img/1.png" alt="..." />
+      </div> 
+
+      <div className="flex justify-center bg-slate-100 w-full h-full">
+        <Image width={1920} height={1080} className="h-auto w-1/2 mt-0" src="/img/2.png" alt="..." />
+      </div> 
+
+      <div className="flex justify-center bg-slate-100 w-full h-full">
+        <Image width={1920} height={1080} className="h-auto w-1/2 mt-0" src="/img/3.png" alt="..." />
+      </div> 
+
+      <div className="flex justify-center bg-slate-100 w-full h-full">
+        <Image width={1920} height={1080} className="h-auto w-1/2 mt-0" src="/img/4.png" alt="..." />
+      </div> 
+
+      <div className="flex justify-center bg-slate-100 w-full h-full">
+        <Image width={1920} height={1080} className="h-auto w-1/2 mt-0" src="/img/5.png" alt="..." />
+      </div>  
+
       </Carousel>
     </div>
   );
